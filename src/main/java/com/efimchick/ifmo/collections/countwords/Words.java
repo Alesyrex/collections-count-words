7d868d286251b2a200379900cd9e399170ac200c
Alesyrex <Alesyrex@gmail.com>
2022-03-27 11:48:15 +0300

    correct result output

--- src/main/java/com/efimchick/ifmo/collections/countwords/Words.java
+++ src/main/java/com/efimchick/ifmo/collections/countwords/Words.java
@@ -8,16 +8,19 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Words {
-    public static final String RESULT_FORMAT = "%s - %d\n";
+    public static final String PAIR_FORMAT = " - ";
+    public static final String LINE_SEPARATOR = "\n";
     public static final int MIN_REPEAT_WORDS = 10;
     private static final Pattern PATTERN_LONG_WORDS = Pattern.compile("\\p{IsAlphabetic}{4,}");
 
     private final Map<String, Integer> mapOfWords = new TreeMap<>();
     private final WordsComparator wordsComparator = new WordsComparator();
+    private final List<String> pairString = new ArrayList<>();
 
     public String countWords(List<String> lines) {
         readToMap(lines);
-        return createResultString(sortMapOfWords()).toString();
+        sortMapOfWords();
+        return createResultString();
     }
 
     private void readToMap(List<String> lines) {
@@ -31,21 +34,17 @@ public class Words {
         }
     }
 
-    private List<Map.Entry<String, Integer>> sortMapOfWords() {
+    private void sortMapOfWords() {
         List<Map.Entry<String, Integer>> listOfWords = new ArrayList<>(mapOfWords.entrySet());
         listOfWords.sort(wordsComparator);
-        return listOfWords;
-    }
-
-    private StringBuilder createResultString(List<Map.Entry<String, Integer>> listOfWords) {
-        StringBuilder result = new StringBuilder();
-
         for (Map.Entry<String, Integer> x : listOfWords) {
             if (x.getValue() >= MIN_REPEAT_WORDS) {
-                result.append(String.format(RESULT_FORMAT, x.getKey(), x.getValue()));
+                pairString.add(String.join(PAIR_FORMAT, x.getKey(), x.getValue().toString()));
             }
         }
-        result.deleteCharAt(result.length() - 1);
-        return result;
+    }
+
+    private String createResultString() {
+        return String.join(LINE_SEPARATOR, pairString);
     }
 }
